35c1e8ebbc5546ac7d9079e1499339f978864414
rubio.viana <rubio.viana@bwtech.com>
2021-08-19 13:33:45 -0300

    Separate endpoints

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^scheduling/$', views.SchedulingList.as_view(), name='scheduling-list'),
+    url(r'^scheduling/search/(?P<pk>[0-9]+)$', views.SchedulingList.as_view(), name='scheduling-list'),
+    url(r'^scheduling/create/$', views.SchedulingCreate.as_view(), name='scheduling-create')
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,12 +1,16 @@
-
 from rest_framework import generics
 from .models import Scheduling
 from .serializers import SchedulingSerializer
 from django_filters import rest_framework as filters
 
-# Create your views here.
-class SchedulingList(generics.ListCreateAPIView):
 
+# Create your views here.
+class SchedulingList(generics.RetrieveAPIView):
+    queryset = Scheduling.objects.all()
+    serializer_class = SchedulingSerializer
+
+
+class SchedulingCreate(generics.CreateAPIView):
     queryset = Scheduling.objects.all()
     serializer_class = SchedulingSerializer
     filter_backends = (filters.DjangoFilterBackend,)
