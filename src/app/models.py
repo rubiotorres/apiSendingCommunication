06b04806269265ad72b6cddde5d64b8b19431544
Rubio Viana <rubiotorres15@gmail.com>
2021-08-19 20:37:34 -0300

    Add basic module test

--- src/app/models.py
+++ src/app/models.py
@@ -5,17 +5,15 @@ from django.db import models
 
 
 class Scheduling(models.Model):
-
     class Meta:
-
         db_table = 'Scheduling'
 
-    date_entry = models.DateTimeField(auto_now_add=True)    
+    date_entry = models.DateTimeField(auto_now_add=True)
     sender = models.CharField(max_length=50)
     date_send = models.DateTimeField()
     receiver = models.CharField(max_length=50)
     message = models.CharField(max_length=200)
-    status = models.CharField(max_length=10,default="Scheduled")
+    status = models.CharField(max_length=10, default="Scheduled")
 
     def __str__(self):
-        return self.title
+        return "This message is going from {} to {}.\n Day: {}".format(self.sender, self.receiver, self.date_send)
